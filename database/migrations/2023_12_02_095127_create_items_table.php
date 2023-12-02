0a27bcfefd9fc62415451179208ca4bd74f8af53
Zai185 <mkhant185@gmail.com>
2023-12-02 23:40:25 +0630

    first commit by backend-setup

---
+++ database/migrations/2023_12_02_095127_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('slug');
+            $table->integer('price');
+            $table->text('description');
+            $table->boolean('hasDiscount');
+            $table->integer('discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};

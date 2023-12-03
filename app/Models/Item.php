6e04c43cb5ae25e6d3f0b44621f2237d92726358
Zai185 <mkhant185@gmail.com>
2023-12-03 15:12:46 +0630

    Add feature Sluggable

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Item extends Model
 {
     use HasFactory;
+    use HasSlug;
+
     protected $fillable = ['name', 'slug',  'price', 'description', 'image_url', 'hasDiscount', 'discount'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Item;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Item::factory(4)->create();
     }
 }
